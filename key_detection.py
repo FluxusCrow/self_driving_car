889338593f869683e42cd437807140ffa978abf6
FluxusCrow <pascal.kroh@web.de>
2022-07-05 23:45:22 +0200

    script to globally detect keyboard inputs

---
+++ key_detection.py
@@ -0,0 +1,29 @@
+from pynput import keyboard
+
+keys=[]
+def on_press(key):
+    try:
+        format(key.char)
+    except AttributeError:
+        format(key)
+    keys.append(key)
+    print(keys)
+
+def on_release(key):
+    format(key)
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+## ...or, in a non-blocking fashion:
+#listener = keyboard.Listener(
+#    on_press=on_press,
+#    on_release=on_release)
+#listener.start()
+
