edb596b3bcd0ad8b33a8046eadf4fa772e253736
FluxusCrow <pascal.kroh@web.de>
2022-07-05 16:20:33 +0200

    releaseKey() is added to end of script to avoid keaboard inputs after ending of script

--- gta_python.py
+++ gta_python.py
@@ -44,7 +44,7 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
                 x_coords = (xyxy[0],xyxy[2])
                 y_coords = (xyxy[1],xyxy[3])
                 A = vstack([x_coords,ones(len(x_coords))]).T
-                m, b = lstsq(A, y_coords)[0]
+                m, b = lstsq(A, y_coords, rcond=None)[0]
 
                 # Calculating our new, and improved, xs
                 x1 = (min_y-b) / m
@@ -103,7 +103,7 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
         l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
         l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
 
-        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2], lane1_id, lane2_id
     except Exception as e:
         print(str(e))
 
@@ -125,8 +125,10 @@ def process_img(image):
     # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
     #                                     rho   theta   thresh  min length, max gap:        
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,      20,       15)
+    m1 = 0
+    m2 = 0
     try:
-        l1, l2 = draw_lanes(original_image,lines)
+        l1, l2, m1,m2 = draw_lanes(original_image,lines)
         cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
         cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
     except Exception as e:
@@ -144,7 +146,30 @@ def process_img(image):
     except Exception as e:
         pass
 
-    return processed_img,original_image
+    return processed_img,original_image, m1, m2
+
+def straight():
+    pressKey("w")
+    releaseKey("a")
+    releaseKey("d")
+
+def left():
+    pressKey("a")
+    releaseKey("w")
+    releaseKey("d")
+    releaseKey("a")
+
+def right():
+    pressKey("d")
+    releaseKey("a")
+    releaseKey("w")
+    releaseKey("d")
+
+def slow_ya_roll():
+    releaseKey("w")
+    releaseKey("a")
+    releaseKey("d")
+
 
 
 def main():
@@ -159,11 +184,22 @@ def main():
         screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
         print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        new_screen,original_image = process_img(screen)
-        cv2.imshow('window', new_screen)
+        new_screen,original_image, m1, m2 = process_img(screen)
+        #cv2.imshow('window', new_screen)
         cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+
+        if m1 < 0 and m2 < 0:
+            right()
+        elif m1 > 0  and m2 > 0:
+            left()
+        else:
+            straight()
+
         #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
+            releaseKey("w")
+            releaseKey("a")
+            releaseKey("d")
             break
 main()
