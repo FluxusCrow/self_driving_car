0e9149acfd9609d6ad642e9b93c7c1a8c00a8f2f
FluxusCrow <pascal.kroh@web.de>
2022-07-07 23:50:27 +0200

    added option for screen grabbing for two screens

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ from grab_screen import process_img
 
 def main():
     
-    for i in list(range(3))[::-1]:
+    for i in list(range(0))[::-1]:
         print(i+1)
         time.sleep(1)
 
     #last_time = time.time()
     with mss.mss() as sct:
     # The screen part to capture
-        monitor = {"top": 65, "left": 75, "width": 800, "height": 600}
+    ## If only one screen is connected
+        #monitor = {"top": 65, "left": 75, "width": 800, "height": 600}
+    ## If multiple screens are connected
+        mon1 = sct.monitors[1]
+        monitor = {"top": mon1["top"] + 65, "left": mon1["left"] + 75, "width": 800, "height": 600}
     while True:
         screen =  np.array(sct.grab(monitor))
         #print('Frame took {} seconds'.format(time.time()-last_time))
         #last_time = time.time()
         new_screen,original_image, m1, m2 = process_img(screen)
-        #cv2.imshow('window', new_screen)
+        cv2.imshow('window', new_screen)
         cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 
         if m1 < 0 and m2 < 0:
@@ -32,7 +36,7 @@ def main():
         else:
             straight()
 
-        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        ##cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             release_keys()
