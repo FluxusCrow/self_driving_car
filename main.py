343b49af54a23751cf294fff841ade5f20b3797a
FluxusCrow <pascal.kroh@web.de>
2022-07-09 22:58:32 +0200

    added f1 as start and esc as stop. fixed color of grabed screen

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import mss
 import mss.tools
 from key_inputs import straight, left, right, release_keys
 from grab_screen import process_img
+from pynput import keyboard
 
+# set up key logging. F1 = start, ESC = end
+def on_press_start(key):
+    if key == keyboard.Key.f1:
+        return False
+
+def on_press_loop(key):
+    if key == keyboard.Key.esc:
+        return False
 
 def main():
     
@@ -21,24 +30,30 @@ def main():
     ## If multiple screens are connected
         mon1 = sct.monitors[1]
         monitor = {"top": mon1["top"] + 65, "left": mon1["left"] + 75, "width": 800, "height": 600}
-    while True:
-        screen =  np.array(sct.grab(monitor))
-        #print('Frame took {} seconds'.format(time.time()-last_time))
-        #last_time = time.time()
-        new_screen,original_image, m1, m2 = process_img(screen)
-        cv2.imshow('window', new_screen)
-        cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    with keyboard.Listener(on_press=on_press_loop) as listener:
+        while listener.running:
+            screen =  np.array(sct.grab(monitor))
+            #print('Frame took {} seconds'.format(time.time()-last_time))
+            #last_time = time.time()
+            new_screen,original_image, m1, m2 = process_img(screen)
+            cv2.imshow('window', new_screen)
+            cv2.imshow('window2', original_image)
+    
+            if m1 < 0 and m2 < 0:
+                right()
+            elif m1 > 0  and m2 > 0:
+                left()
+            else:
+                straight()
+    
+            ##cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                release_keys()
+                break
 
-        if m1 < 0 and m2 < 0:
-            right()
-        elif m1 > 0  and m2 > 0:
-            left()
-        else:
-            straight()
+print("The machine is ready. Press F1 to start!")
+with keyboard.Listener(on_press=on_press_start) as listener:
+    listener.join()
 
-        ##cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            release_keys()
-            break
 main()
